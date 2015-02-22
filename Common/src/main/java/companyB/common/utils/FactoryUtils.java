1e82787120a1df00c4998cdf485fee0dbeb2384e
charlie <deltafront@gmail.com>
2015-02-22 09:34:53 -0800

    pushing

--- Common/src/main/java/companyB/common/utils/FactoryUtils.java
+++ Common/src/main/java/companyB/common/utils/FactoryUtils.java
@@ -1,79 +1,1 @@
-package companyB.common.utils;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * <strong>THE</strong> Object Instantiation Factory!
- *
- * @author cburrell deltafront@gmail.com
- * @version 1.0
- */
-public abstract class FactoryUtils
-{
-    private static Logger log;
-    private static Map<String, Object> objects;
-
-    static
-    {
-        if (log == null)
-        {
-            log = LoggerFactory.getLogger(FactoryUtils.class);
-        }
-        if (objects == null)
-        {
-            objects = new HashMap<String, Object>();
-        }
-    }
-
-    /**
-     * Returns an object that is of the type specified by the class name, or null
-     * if that class cannot be loaded.
-     *
-     * @param class_name Fully qualified class name.
-     * @return object of type, or null
-     * @since 1.0
-     */
-    public static Object loadObject(String class_name)
-    {
-        return loadObject(class_name, false);
-    }
-
-    /**
-     * Returns an object that is of the type specified by the class name, or null
-     * if that class cannot be loaded. If the static indicator is set to 'true', then this instance will either be
-     * loaded from an internal map, or, if it isn't present, it will be loaded into the map for future retrieval
-     *
-     * @param class_name Fully qualified class name.
-     * @param is_static  Whether or not to load class as a singleton.
-     * @return object of type, or null
-     * @since 1.0
-     */
-    public static Object loadObject(String class_name, boolean is_static)
-    {
-        Object out = null;
-        try
-        {
-            if (is_static)
-            {
-                out = objects.get(class_name);
-            }
-            if (out == null)
-            {
-                out = Class.forName(class_name).newInstance();
-                if (is_static)
-                {
-                    objects.put(class_name, out);
-                }
-            }
-        }
-        catch (Exception e)
-        {
-            log.error(e.getMessage(), e);
-        }
-        return out;
-    }
-}
+package companyB.common.utils;import org.slf4j.Logger;import org.slf4j.LoggerFactory;import java.util.HashMap;import java.util.Map;/** * <strong>THE</strong> Object Instantiation Factory! * * @author cburrell deltafront@gmail.com * @version 1.0 */public abstract class FactoryUtils{    private static Logger log;    private static Map<String, Object> objects;    static    {        if (log == null)        {            log = LoggerFactory.getLogger(FactoryUtils.class);        }        if (objects == null)        {            objects = new HashMap<String, Object>();        }    }    /**     * Returns an object that is of the type specified by the class name, or null     * if that class cannot be loaded.     *     * @param class_name Fully qualified class name.     * @return object of type, or null     * @since 1.0     */    public static Object loadObject(String class_name)    {        return loadObject(class_name, false);    }    /**     * Returns an object that is of the type specified by the class name, or null     * if that class cannot be loaded. If the static indicator is set to 'true', then this instance will either be     * loaded from an internal map, or, if it isn't present, it will be loaded into the map for future retrieval     *     * @param class_name Fully qualified class name.     * @param is_static  Whether or not to load class as a singleton.     * @return object of type, or null     * @since 1.0     */    public static Object loadObject(String class_name, boolean is_static)    {        Object out = null;        try        {            if (is_static)            {                out = objects.get(class_name);            }            if (out == null)            {                out = Class.forName(class_name).newInstance();                if (is_static)                {                    objects.put(class_name, out);                }            }        }        catch (Exception e)        {            log.error(e.getMessage(), e);        }        return out;    }}
--- Common/src/main/java/companyB/common/utils/PropertiesUtils.java
+++ Common/src/main/java/companyB/common/utils/PropertiesUtils.java
@@ -1,108 +1,1 @@
-package companyB.common.utils;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
-
-import static org.apache.commons.lang3.StringUtils.isEmpty;
-
-
-/**
- * This class provides a simple wrapper for java.util.Properties.
- * It allows a property to be retrieved using a single-line of code.
- * This class is designed to work with a key=value type properties file.
- *
- * @author C.A. Burrell deltafront@gmail.com
- * @version 1.0
- */
-@SuppressWarnings("PMD.UselessParentheses")
-public abstract class PropertiesUtils
-{
-    private static Properties props;
-    private static Map<String, Properties> props_map;
-
-    static
-    {
-        if (props_map == null)
-        {
-            props_map = Collections.synchronizedMap(new HashMap<String, Properties>());
-        }
-    }
-
-    /**
-     * Returns the property value associated with 'key'.
-     *
-     * @param path location of properties file
-     * @param key  attribute to get property value for
-     * @return value associated with key, or null
-     * @since 1.0
-     */
-    public static String getProperty(String path, String key)
-    {
-        String out = null;
-        loadProps(path);
-        out = (isEmpty(key)) ? null : props.getProperty(key);
-        return out;
-    }
-
-    /**
-     * Returns a map of all properties contained within field located at 'path'
-     *
-     * @param path path at which desired file is located
-     * @return HashMap of all properties contained within field located at 'path'
-     * @since 1.0
-     */
-    @SuppressWarnings({"unchecked", "WhileLoopReplaceableByForEach"})
-    public static HashMap<String, String> getProperties(String path)
-    {
-        HashMap<String, String> map = new HashMap<String, String>();
-        if (!isEmpty(path) && !map.containsKey(path))
-        {
-            loadProps(path);
-        }
-        if (!isEmpty(path))
-        {
-            Properties _props = props_map.get(path);
-            java.util.Set keys_set = _props.keySet();
-            java.util.Iterator keys = keys_set.iterator();
-            while (keys.hasNext())
-            {
-                String key = (String) keys.next();
-                String value = (String) _props.get(key);
-                map.put(key, value);
-            }
-        }
-        else
-        {
-            map = null;
-        }
-        return map;
-    }
-
-    //loads the properties from a properties file
-    private static void loadProps(String path)
-    {
-        props = new Properties();
-        try
-        {
-            if (isEmpty(path))
-            {
-                throw new IOException("Filename must be provided");
-            }
-            File file = new File(path);
-            FileInputStream fis = new FileInputStream(file);
-            props.load(fis);
-            props_map.put(path, props);
-        }
-        catch (IOException e)
-        {
-            throw new IllegalStateException(
-                    "ClasspathPropertiesLoader: Unable to find the file on the classpath:\n",
-                    e);
-        }
-    }
-}
+package companyB.common.utils;import java.io.File;import java.io.FileInputStream;import java.io.IOException;import java.util.Collections;import java.util.HashMap;import java.util.Map;import java.util.Properties;import static org.apache.commons.lang3.StringUtils.isEmpty;/** * This class provides a simple wrapper for java.util.Properties. * It allows a property to be retrieved using a single-line of code. * This class is designed to work with a key=value type properties file. * * @author C.A. Burrell deltafront@gmail.com * @version 1.0 */@SuppressWarnings("PMD.UselessParentheses")public abstract class PropertiesUtils{    private static Properties props;    private static Map<String, Properties> props_map;    static    {        if (props_map == null)        {            props_map = Collections.synchronizedMap(new HashMap<String, Properties>());        }    }    /**     * Returns the property value associated with 'key'.     *     * @param path location of properties file     * @param key  attribute to get property value for     * @return value associated with key, or null     * @since 1.0     */    public static String getProperty(String path, String key)    {        String out = null;        loadProps(path);        out = (isEmpty(key)) ? null : props.getProperty(key);        return out;    }    /**     * Returns a map of all properties contained within field located at 'path'     *     * @param path path at which desired file is located     * @return HashMap of all properties contained within field located at 'path'     * @since 1.0     */    @SuppressWarnings({"unchecked", "WhileLoopReplaceableByForEach"})    public static HashMap<String, String> getProperties(String path)    {        HashMap<String, String> map = new HashMap<String, String>();        if (!isEmpty(path) && !map.containsKey(path))        {            loadProps(path);        }        if (!isEmpty(path))        {            Properties _props = props_map.get(path);            java.util.Set keys_set = _props.keySet();            java.util.Iterator keys = keys_set.iterator();            while (keys.hasNext())            {                String key = (String) keys.next();                String value = (String) _props.get(key);                map.put(key, value);            }        }        else        {            map = null;        }        return map;    }    //loads the properties from a properties file    private static void loadProps(String path)    {        props = new Properties();        try        {            if (isEmpty(path))            {                throw new IOException("Filename must be provided");            }            File file = new File(path);            FileInputStream fis = new FileInputStream(file);            props.load(fis);            props_map.put(path, props);        }        catch (IOException e)        {            throw new IllegalStateException(                    "ClasspathPropertiesLoader: Unable to find the file on the classpath:\n",                    e);        }    }}
