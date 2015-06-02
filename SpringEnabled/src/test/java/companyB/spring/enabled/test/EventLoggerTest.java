b4b1471f0fcdfa5c383dfa573dd05570671b781e
charlie <deltafront@gmail.com>
2015-06-01 22:43:02 -0700

    Added more tests and functonality to sprig stuff

--- SpringEnabled/src/test/java/companyB/spring/enabled/test/EventLoggerTest.java
+++ SpringEnabled/src/test/java/companyB/spring/enabled/test/EventLoggerTest.java
@@ -1,7 +1,12 @@
 package companyB.spring.enabled.test;
 
+import companyB.eventlogger.EventLog;
+import companyB.eventlogger.EventLogger;
+import companyB.eventlogger.LogMessageFormatter;
 import companyB.spring.eventLogger.EventLoggerBeanProcessor;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
 
 /**
  * Created by Charles Burrell (deltafront@gmail.com).
@@ -9,19 +14,43 @@ import org.testng.annotations.Test;
 @Test(groups = {"unit","spring","event.logger.bean.processor"})
 public class EventLoggerTest
 {
-
-
-    public void happyPath()
+    public void testBefore()
     {
+        process(true);
+    }
 
+    public void testAfter()
+    {
+        process(false);
     }
     private void process(boolean doBefore)
     {
         EventLoggerBeanProcessor processor = new EventLoggerBeanProcessor();
-        //TODO - write rest of class.
+        EventLoggerEnabledTestClass eventLoggerEnabledTestClass = new EventLoggerEnabledTestClass();
+        eventLoggerEnabledTestClass = (doBefore) ?
+                (EventLoggerEnabledTestClass)processor.postProcessBeforeInitialization(eventLoggerEnabledTestClass, "Test") :
+                (EventLoggerEnabledTestClass)processor.postProcessAfterInitialization(eventLoggerEnabledTestClass,"Test");
+        assertNotNull(eventLoggerEnabledTestClass);
+        if(doBefore)
+        {
+            EventLogger eventLogger = eventLoggerEnabledTestClass.eventLogger;
+            assertNotNull(eventLogger);
+            assertEquals("Foo",eventLogger.getName());
+            LogMessageFormatter fromDecorated = eventLoggerEnabledTestClass.eventLogger.getLogMessageFormatter();
+            assertNotNull(fromDecorated);
+            assertTrue(fromDecorated instanceof AdHocLogMessageFormatter);
+        }
+        else
+        {
+            assertNull(eventLoggerEnabledTestClass.eventLogger);
+        }
+        assertNull(eventLoggerEnabledTestClass.nonEventLogger);
     }
-    public static class EventLoggerEnabledTestClass
-    {
-
-    }
+}
+class EventLoggerEnabledTestClass
+{
+    @EventLog(name = "Foo", logMessageFormatter = AdHocLogMessageFormatter.class)
+    EventLogger eventLogger;
+    String test;
+    EventLogger nonEventLogger;
 }
