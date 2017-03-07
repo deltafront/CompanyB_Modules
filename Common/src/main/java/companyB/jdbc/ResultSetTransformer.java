d2b1bec5f444d29c53425c0e4496948ca86625ca
chburrell <chburrell@expedia.com>
2017-03-07 10:40:16 -0800

    Finishing up jdbc

--- Common/src/main/java/companyB/jdbc/ResultSetTransformer.java
+++ Common/src/main/java/companyB/jdbc/ResultSetTransformer.java
@@ -3,10 +3,16 @@ package companyB.jdbc;
 import java.sql.ResultSet;
 
 /**
+ * Generic contract for transforming the contents of a result set to a specific object instance.
  * @author C.A. Burrell (deltafront@gmail.com)
  * @version 1.0.0
  */
 public interface ResultSetTransformer<TargetClass>
 {
+    /**
+     * Transforms the contents of this result set into a specific target class.
+     * @param resultSet Result set that contains the results of a SQL operation.
+     * @return Instance of TargetClass.
+     */
     TargetClass fromResultSet(ResultSet resultSet);
 }
